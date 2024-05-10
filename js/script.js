dcd016cc3ca47a37893c1318b41e98e427eacad3
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-10 17:55:30 +0000

    save the last changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c)2024 George Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George Khalil
+// Created on: May 2024
 // This file contains the JS functions for index.html
