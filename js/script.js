9e21e7bab76158fee84181f33b8ff2f5f7db316d
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-10 18:26:22 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@
 // Created by: George Khalil
 // Created on: May 2024
 // This file contains the JS functions for index.html
+
+
+function longDivision() {
+    const dividend = parseInt(document.getElementById("dividend").value);
+    const divisor = parseInt(document.getElementById("divisor").value);
+    let quotient = 0;
+    let remainder = dividend;
+    let steps = "";
+
+    for (let i = 0; i < 3; i++) {
+        if (remainder >= divisor) {
+            steps += `${remainder} - ${divisor} = ${remainder - divisor} ☚ ${i === 0 ? "first" : i === 1 ? "second" : "third"} time<br>`;
+            remainder -= divisor;
+            quotient += 1;
+        }
+    }
+
+    steps += `Therefore ${dividend} / ${divisor} = ${quotient} R ${remainder}`;
+    document.getElementById("result").innerHTML = steps;
+}
